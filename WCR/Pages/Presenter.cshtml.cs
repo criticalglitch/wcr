be833afe194c0b65ae38ef6a51a0d994fcf7d7ec
Rebecca McCullough <criticalglitch@users.noreply.github.com>
2024-07-01 22:05:15 -0400

    Updated references from Viewer to Presenter

--- WCR/Pages/Presenter.cshtml.cs
+++ WCR/Pages/Presenter.cshtml.cs
@@ -3,7 +3,7 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace WCR.Pages
 {
-    public class ViewerModel : PageModel
+    public class PresenterModel : PageModel
     {
         public void OnGet()
         {
